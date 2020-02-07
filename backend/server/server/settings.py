9aa66d4f8a070f0d3f1d8d04687f71a6b2c285fd
Phan Dang <phan@Phans-MacBook-Pro-4.local>
2020-02-06 18:10:05 -0800

    endpoints models

--- backend/server/server/settings.py
+++ backend/server/server/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # apps
+    'apps.endpoints',
+    'rest_framework'
+
 ]
 
 MIDDLEWARE = [
